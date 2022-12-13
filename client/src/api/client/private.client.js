0d9e26eccb00a5c96a9d7c817301555b875d38a8
Tran Anh Tuat <tuattrananh@gmail.com>
2022-12-13 22:38:17 +0900

    update api link

--- client/src/api/client/private.client.js
+++ client/src/api/client/private.client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseURL = "http://127.0.0.1:5000/api/v1/";
+const baseURL = "https://moonflix-api.vercel.app/api/v1/";
 
 const privateClient = axios.create({
   baseURL,
