21f6821c5b99bdc7553db564aad4619db8ea53b4
CotChi <chiennguyen.170921@gmail.com>
2022-12-22 17:33:05 +0700

    update lost content in detail apge

--- client/src/pages/MediaDetail.jsx
+++ client/src/pages/MediaDetail.jsx
@@ -48,7 +48,11 @@ const MediaDetail = () => {
     window.scrollTo(0, 0);
     const getMedia = async () => {
       dispatch(setGlobalLoading(true));
-      const { response, err } = await mediaApi.getDetail({ mediaType, mediaId });
+      const { response, err } = await mediaApi.getDetail({
+        mediaType,
+        mediaId,
+      });
+
       dispatch(setGlobalLoading(false));
 
       if (response) {
@@ -80,7 +84,7 @@ const MediaDetail = () => {
       mediaTitle: media.title || media.name,
       mediaType: mediaType,
       mediaPoster: media.poster_path,
-      mediaRate: media.vote_average
+      mediaRate: media.vote_average,
     };
 
     const { response, err } = await favoriteApi.add(body);
@@ -99,9 +103,13 @@ const MediaDetail = () => {
     if (onRequest) return;
     setOnRequest(true);
 
-    const favorite = listFavorites.find(e => e.mediaId.toString() === media.id.toString());
+    const favorite = listFavorites.find(
+      (e) => e.mediaId.toString() === media.id.toString()
+    );
 
-    const { response, err } = await favoriteApi.remove({ favoriteId: favorite.id });
+    const { response, err } = await favoriteApi.remove({
+      favoriteId: favorite.id,
+    });
 
     setOnRequest(false);
 
@@ -113,162 +121,219 @@ const MediaDetail = () => {
     }
   };
 
-  return (
-    media ? (
-      <>
-        <ImageHeader imgPath={tmdbConfigs.backdropPath(media.backdrop_path || media.poster_path)} />
-        <Box sx={{
+  return media ? (
+    <>
+      <ImageHeader
+        imgPath={tmdbConfigs.backdropPath(
+          media.backdrop_path || media.poster_path
+        )}
+      />
+      <Box
+        sx={{
           color: "primary.contrastText",
-          ...uiConfigs.style.mainContent
-        }}>
-          {/* media content */}
-          <Box sx={{
-            marginTop: { xs: "-10rem", md: "-15rem", lg: "-20rem" }
-          }}>
-            <Box sx={{
+          ...uiConfigs.style.mainContent,
+        }}
+      >
+        {/* media content */}
+        <Box
+          sx={{
+            marginTop: { xs: "-10rem", md: "-15rem", lg: "-20rem" },
+          }}
+        >
+          <Box
+            sx={{
               display: "flex",
-              flexDirection: { md: "row", xs: "column" }
-            }}>
-              {/* poster */}
-              <Box sx={{
+              flexDirection: { md: "row", xs: "column" },
+            }}
+          >
+            {/* poster */}
+            <Box
+              sx={{
                 width: { xs: "70%", sm: "50%", md: "40%" },
-                margin: { xs: "0 auto 2rem", md: "0 2rem 0 0" }
-              }}>
-                <Box sx={{
+                margin: { xs: "0 auto 2rem", md: "0 2rem 0 0" },
+              }}
+            >
+              <Box
+                sx={{
                   paddingTop: "140%",
-                  ...uiConfigs.style.backgroundImage(tmdbConfigs.posterPath(media.poster_path || media.backdrop_path))
-                }} />
-              </Box>
-              {/* poster */}
+                  ...uiConfigs.style.backgroundImage(
+                    tmdbConfigs.posterPath(
+                      media.poster_path || media.backdrop_path
+                    )
+                  ),
+                }}
+              />
+            </Box>
+            {/* poster */}
 
-              {/* media info */}
-              <Box sx={{
+            {/* media info */}
+            <Box
+              sx={{
                 width: { xs: "100%", md: "60%" },
-                color: "text.primary"
-              }}>
-                <Stack spacing={5}>
-                  {/* title */}
-                  <Typography
-                    variant="h4"
-                    fontSize={{ xs: "2rem", md: "2rem", lg: "4rem" }}
-                    fontWeight="700"
-                    sx={{ ...uiConfigs.style.typoLines(2, "left") }}
-                  >
-                    {`${media.title || media.name} ${mediaType === tmdbConfigs.mediaType.movie ? media.release_date.split("-")[0] : media.first_air_date.split("-")[0]}`}
-                  </Typography>
-                  {/* title */}
+                color: "text.primary",
+              }}
+            >
+              <Stack spacing={5}>
+                {/* title */}
+                <Typography
+                  variant="h4"
+                  fontSize={{ xs: "2rem", md: "2rem", lg: "4rem" }}
+                  fontWeight="700"
+                  sx={{ ...uiConfigs.style.typoLines(2, "left") }}
+                >
+                  {`${media.title || media.name} ${
+                    mediaType === tmdbConfigs.mediaType.movie
+                      ? media.release_date.split("-")[0]
+                      : media.first_air_date.split("-")[0]
+                  }`}
+                </Typography>
+                {/* title */}
 
-                  {/* rate and genres */}
-                  <Stack direction="row" spacing={1} alignItems="center">
-                    {/* rate */}
-                    <CircularRate value={media.vote_average} />
-                    {/* rate */}
-                    <Divider orientation="vertical" />
-                    {/* genres */}
-                    {genres.map((genre, index) => (
-                      <Chip
-                        label={genre.name}
-                        variant="filled"
-                        color="primary"
-                        key={index}
-                      />
-                    ))}
-                    {/* genres */}
-                  </Stack>
-                  {/* rate and genres */}
-
-                  {/* overview */}
-                  <Typography
-                    variant="body1"
-                    sx={{ ...uiConfigs.style.typoLines(5) }}
-                  >
-                    {media.overview}
-                  </Typography>
-                  {/* overview */}
-
-                  {/* buttons */}
-                  <Stack direction="row" spacing={1}>
-                    <LoadingButton
-                      variant="text"
-                      sx={{
-                        width: "max-content",
-                        "& .MuiButon-starIcon": { marginRight: "0" }
-                      }}
-                      size="large"
-                      startIcon={isFavorite ? <FavoriteIcon /> : <FavoriteBorderOutlinedIcon />}
-                      loadingPosition="start"
-                      loading={onRequest}
-                      onClick={onFavoriteClick}
+                {/* rate and genres */}
+                <Stack direction="row" spacing={1} alignItems="center">
+                  {/* rate */}
+                  <CircularRate value={media.vote_average} />
+                  {/* rate */}
+                  <Divider orientation="vertical" />
+                  {/* genres */}
+                  {genres.map((genre, index) => (
+                    <Chip
+                      label={genre.name}
+                      variant="filled"
+                      color="primary"
+                      key={index}
                     />
-                    <Button
-                      variant="contained"
-                      sx={{ width: "max-content" }}
-                      size="large"
-                      startIcon={<PlayArrowIcon />}
-                      onClick={() => videoRef.current.scrollIntoView()}
-                    >
-                      watch now
-                    </Button>
-                  </Stack>
-                  {/* buttons */}
+                  ))}
+                  {/* genres */}
+                </Stack>
+                {/* rate and genres */}
 
-                  {/* cast */}
+                {/* overview */}
+                <Typography
+                  variant="body1"
+                  sx={{ ...uiConfigs.style.typoLines(5) }}
+                >
+                  {media.overview}
+                </Typography>
+                {/* overview */}
+
+                {/* buttons */}
+                <Stack direction="row" spacing={1}>
+                  <LoadingButton
+                    variant="text"
+                    sx={{
+                      width: "max-content",
+                      "& .MuiButon-starIcon": { marginRight: "0" },
+                    }}
+                    size="large"
+                    startIcon={
+                      isFavorite ? (
+                        <FavoriteIcon />
+                      ) : (
+                        <FavoriteBorderOutlinedIcon />
+                      )
+                    }
+                    loadingPosition="start"
+                    loading={onRequest}
+                    onClick={onFavoriteClick}
+                  />
+                  <Button
+                    variant="contained"
+                    sx={{ width: "max-content" }}
+                    size="large"
+                    startIcon={<PlayArrowIcon />}
+                    onClick={() => videoRef.current.scrollIntoView()}
+                  >
+                    watch now
+                  </Button>
+                </Stack>
+                {/* buttons */}
+
+                {/* cast */}
+                {media.credits.cast < 1 ? (
+                  <Container header="Cast">
+                    <div className="swiper-wrapper">
+                      {" "}
+                      There is no cast for this content.{" "}
+                    </div>
+                  </Container>
+                ) : (
                   <Container header="Cast">
                     <CastSlide casts={media.credits.cast} />
                   </Container>
-                  {/* cast */}
-                </Stack>
-              </Box>
-              {/* media info */}
-            </Box>
-          </Box>
-          {/* media content */}
+                )}
 
-          {/* media videos */}
+                {/* cast */}
+              </Stack>
+            </Box>
+            {/* media info */}
+          </Box>
+        </Box>
+        {/* media content */}
+
+        {/* media videos */}
+
+        {media.videos.results < 1 ? (
           <div ref={videoRef} style={{ paddingTop: "2rem" }}>
             <Container header="Videos">
-              <MediaVideosSlide videos={[...media.videos.results].splice(0, 5)} />
+              <div className="swiper-wrapper">
+                The video of the content has been lost, sorry for the
+                inconvenience.{" "}
+              </div>
             </Container>
           </div>
-          {/* media videos */}
-
-          {/* media backdrop */}
-          {media.images.backdrops.length > 0 && (
-            <Container header="backdrops">
-              <BackdropSlide backdrops={media.images.backdrops} />
-            </Container>
-          )}
-          {/* media backdrop */}
-
-          {/* media posters */}
-          {media.images.posters.length > 0 && (
-            <Container header="posters">
-              <PosterSlide posters={media.images.posters} />
-            </Container>
-          )}
-          {/* media posters */}
-
-          {/* media reviews */}
-          <MediaReview reviews={media.reviews} media={media} mediaType={mediaType} />
-          {/* media reviews */}
-
-          {/* media recommendation */}
-          <Container header="you may also like">
-            {media.recommend.length > 0 && (
-              <RecommendSlide medias={media.recommend} mediaType={mediaType} />
-            )}
-            {media.recommend.length === 0 && (
-              <MediaSlide
-                mediaType={mediaType}
-                mediaCategory={tmdbConfigs.mediaCategory.top_rated}
+        ) : (
+          <div ref={videoRef} style={{ paddingTop: "2rem" }}>
+            <Container header="Videos">
+              <MediaVideosSlide
+                videos={[...media.videos.results].splice(0, 5)}
               />
-            )}
+            </Container>
+          </div>
+        )}
+
+        {/* media videos */}
+
+        {/* media backdrop */}
+        {media.images.backdrops.length > 0 && (
+          <Container header="backdrops">
+            <BackdropSlide backdrops={media.images.backdrops} />
           </Container>
-          {/* media recommendation */}
-        </Box>
-      </>
-    ) : null
-  );
+        )}
+        {/* media backdrop */}
+
+        {/* media posters */}
+        {media.images.posters.length > 0 && (
+          <Container header="posters">
+            <PosterSlide posters={media.images.posters} />
+          </Container>
+        )}
+        {/* media posters */}
+
+        {/* media reviews */}
+        <MediaReview
+          reviews={media.reviews}
+          media={media}
+          mediaType={mediaType}
+        />
+        {/* media reviews */}
+
+        {/* media recommendation */}
+        <Container header="you may also like">
+          {media.recommend.length > 0 && (
+            <RecommendSlide medias={media.recommend} mediaType={mediaType} />
+          )}
+          {media.recommend.length === 0 && (
+            <MediaSlide
+              mediaType={mediaType}
+              mediaCategory={tmdbConfigs.mediaCategory.top_rated}
+            />
+          )}
+        </Container>
+        {/* media recommendation */}
+      </Box>
+    </>
+  ) : null;
 };
 
 export default MediaDetail;
